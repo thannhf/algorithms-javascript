2dd54a9df7e3199200e8f10c83cf0409eec62d50
thannhf <112296523+thannhf@users.noreply.github.com>
2024-02-27 17:33:12 +0700

    Update Genetic Algorithm.js

--- Genetic Algorithm.js
+++ Genetic Algorithm.js
@@ -1,1 +1,12 @@
-
+/*
+Khi Sinh học áp dụng vào Lập trình: Giải thuật di truyền - Genetic Algorithm dựa vào quá trình tiến hóa của mọi sinh vật trong vũ trụ
+Có nhiều bạn hay đặt câu hỏi: Toán học được áp dụng nhiều vào lập trình. Vậy Hóa Học, Vật Lý, Sinh học,... thì như thế nào? Thực tế, có rất nhiều thuật toán được phát triển dựa trên các nguyên lý ở các lĩnh vực khoa học nói trên. Một trong những thuật toán dựa trên các nguyên lý di truyền trong sinh học, được áp dụng vào các bài toán tối ưu là "Giải thuật di truyền".
+Giải thuật di truyền (GA) là một nhánh của thuật toán tối ưu hoá và tìm kiếm dựa trên nguyên lý lựa chọn tự nhiên của Darwin. Đây cũng là một trong những thuật toán có thể bạn sẽ học qua trong các môn học liên quan đến Trí Tuệ Nhân Tạo (AI). GA mô phỏng quá trình tiến hóa tự nhiên để tạo ra các giải pháp tối ưu cho bài toán. Các "sinh vật" ảo trong GA được biểu diễn qua chuỗi gen (thường là mảng hoặc string), và một hàm đánh giá "fitness" để xác định mức độ thích nghi với môi trường (bài toán). Ở đây làm cách việc diễn ra:
+Khởi Tạo: Tạo ra một dân số ban đầu của các cá thể ngẫu nhiên.
+Lựa Chọn: Đánh giá mức độ thích nghi của các cá thể và chọn ra những cá thể "mạnh" nhất.
+Giao Phối (Crossover): Kết hợp gen của các cá thể đã chọn để tạo ra "con cái".
+Đột Biến (Mutation): Đưa ra những thay đổi nhỏ trong các gen để duy trì đa dạng di truyền.
+Lặp Lại: Quá trình này được lặp đi lặp lại qua nhiều thế hệ cho đến khi đạt được cá thể tối ưu hoặc sau một số thế hệ nhất định.
+Thuật toán này không chỉ thú vị mà còn vô cùng mạnh mẽ, có thể áp dụng để giải quyết nhiều loại bài toán từ lập trình đến kinh tế và tâm lý học. Ví dụ, bạn muốn tạo ra một lược đồ màu sắc cho trang web? GA có thể giúp bạn tối ưu sự kết hợp màu sắc dựa trên nguyên tắc thẩm mỹ. Hoặc bạn muốn tối ưu hóa một đặc tính của một vật thể bay? GA cũng có thể giúp bạn "tiến hoá" ra thiết kế hoàn hảo nhất.
+Nếu bạn muốn tìm đến một chút thách thức cho bản thân với một thuật toán vừa có tính ứng dụng, vừa có thể cho bạn những trải nghiệm thú vị đến từ quá trình tiến hóa, thì Genetic Algorithm chính là lựa chọn không thể tuyệt vời hơn!
+*/
